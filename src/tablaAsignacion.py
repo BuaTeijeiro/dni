e607aefd2f82d2d0b484404d48e1ce0b358abd1f
davidbua <bua.teijeiro.david@gmail.com>
2024-01-23 13:13:52 +0100

    feat(tablaAsignacion.py): Crear la clase TablaAsignacion y definir se printado

---
+++ src/tablaAsignacion.py
@@ -0,0 +1,37 @@
+
+
+class TablaAsignacion:
+    def __init__(self):
+        self.tabla = [
+            "T",
+            "R",
+            "W",
+            "A",
+            "G",
+            "M",
+            "Y",
+            "F",
+            "P",
+            "D",
+            "X",
+            "B",
+            "N",
+            "J",
+            "Z",
+            "S",
+            "Q",
+            "V",
+            "H",
+            "L",
+            "C",
+            "K",
+            "E",
+        ]
+        
+    def __repr__(self):
+        return " ".join(self.tabla)
+        
+if __name__ == '__main__':
+    tabla = TablaAsignacion()
+    print(tabla)
+    
