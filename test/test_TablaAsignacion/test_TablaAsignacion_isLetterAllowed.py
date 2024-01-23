8f07008172a476d4b63fbda32f36a5d79ca10ea8
davidbua <bua.teijeiro.david@gmail.com>
2024-01-23 13:58:35 +0100

    test(test_TablaAsignacion_isLetterAllowed): Añadir tests de función isLetterAllowed con letra válida

--- test/test_TablaAsignacion/test_TablaAsignacion_isLetterAllowed.py
+++ test/test_TablaAsignacion/test_TablaAsignacion_isLetterAllowed.py
@@ -2,6 +2,10 @@ from src.tablaAsignacion import TablaAsignacion
 import pytest
 
 @pytest.mark.letterAllowed
-def test_isLetterAllowed():
+def test_isLetterAllowed_allowed():
     tabla = TablaAsignacion()
     assert tabla.isLetterAllowed('A')
+    assert tabla.isLetterAllowed('B')
+    assert tabla.isLetterAllowed('F')
+    assert tabla.isLetterAllowed('L')
+    assert tabla.isLetterAllowed('X')
